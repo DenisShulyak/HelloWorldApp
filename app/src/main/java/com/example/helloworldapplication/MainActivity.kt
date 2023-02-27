2e3df94e65340a908f9aaa149cbc7230251ef0db
denis <dshulyak@pminst.ru>
2023-02-27 23:22:52 +0500

    1. Добавление инпута 2. Вывод информации в TextView по нажатию кнопки

--- app/src/main/java/com/example/helloworldapplication/MainActivity.kt
+++ app/src/main/java/com/example/helloworldapplication/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.core.view.isVisible
 import com.example.helloworldapplication.databinding.ActivityMainBinding
@@ -12,27 +13,20 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var textView: TextView
     private lateinit var button: Button
+    private lateinit var inputText: EditText
     private lateinit var binding: ActivityMainBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         textView = binding.textHello
-        textView.visibility = View.INVISIBLE
         button = binding.buttonShow
+        inputText = binding.editText
     }
 
     fun clickButton(view: View) {
-        if(textView.isVisible)
-        {
-            textView.visibility = View.INVISIBLE
-            button.text = "Отоброзить"
-        }
-        else
-        {
-            textView.visibility = View.VISIBLE
-            button.text = "СКРЫТЬ"
-        }
+        textView.text = inputText.text
     }
 }
